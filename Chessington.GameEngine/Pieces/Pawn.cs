80d134bbaaf6964b0767391334159995450cebc7
MattEnglish <31655162+MattEnglish@users.noreply.github.com>
2017-09-13 11:08:43 +0100

    made prawns first move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,30 +1,52 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
         private Player owner;
+        private bool hasMoved = false;
 
         public Pawn(Player player)
             : base(player)
         {
-            owner = player;
+            owner = player;           
         }
+
+        public override void MoveTo(Board board, Square newSquare)
+        {
+            hasMoved = true;
+            base.MoveTo(board,newSquare);
+        }
+        
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
             var currentSquare = board.FindPiece(this);
             var squares = new List<Square>();
+            
+            
 
             if (this.owner == Player.White)
             {
-                squares.Add(new Square(currentSquare.Row-1,currentSquare.Col));
+                if (!hasMoved)
+                {
+                    squares.Add(new Square(currentSquare.Row - 2, currentSquare.Col));
+                }
+
+                squares.Add(new Square(currentSquare.Row - 1, currentSquare.Col));
             }
             else
             {
-                squares.Add(new Square(currentSquare.Row+1, currentSquare.Col));
+                if (!hasMoved)
+                {
+                    squares.Add(new Square(currentSquare.Row + 2, currentSquare.Col));
+                }
+
+                squares.Add(new Square(currentSquare.Row + 1, currentSquare.Col));
+
             }
 
             return squares;
