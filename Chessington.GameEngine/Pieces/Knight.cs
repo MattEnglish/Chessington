52af941dc7a027dc65a60f406fcb9c9517243e92
MattEnglish <31655162+MattEnglish@users.noreply.github.com>
2017-09-13 13:42:06 +0100

    Knights move

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,20 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board, Square currentSquare)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> squares = new List<Square>();
+            int[,] ls = new int[8, 2] { { 1, 2 }, { 2, 1 }, { -1, 2 }, { 2, -1 }, { -2, 1 }, { 1, -2 }, { -1, -2 }, { -2, -1 } };
+            for (int i = 0; i < 8; i++)
+            {
+                squares.Add(new Square(currentSquare.Row + ls[i, 0], currentSquare.Col + ls[i, 1]));
+            }
+            squares.RemoveAll(s => !Board.IsSquareInBounds(s));
+            return squares;
         }
+        /*
+        private IEnumerable<Square> GetAllLMoves(Square currentSquare)
+        {
+           
+        }
+        */
     }
 }
