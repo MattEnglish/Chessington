f08ca9be86da876fa0286ac5a8ce840949816fbb
MattEnglish <31655162+MattEnglish@users.noreply.github.com>
2017-09-13 11:18:03 +0100

    made rooks move

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var squares = new List<Square>();
+
+            for (int row = 0; row < 8; row++)
+            {
+                if (row != currentSquare.Row)
+                {
+                    squares.Add(new Square(row, currentSquare.Col));
+                }
+            }
+            for (int col = 0; col < 8; col++)
+            {
+                if (col != currentSquare.Row)
+                {
+                    squares.Add(new Square(currentSquare.Row, col));
+                }
+            }
+            return squares;
         }
     }
 }
