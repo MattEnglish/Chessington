19ae38abc44e98cbc55075cb8af5d57c66a5193a
MattEnglish <31655162+MattEnglish@users.noreply.github.com>
2017-09-13 10:43:55 +0100

    made prawns move

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,12 +5,29 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
-            : base(player) { }
+        private Player owner;
+
+        public Pawn(Player player)
+            : base(player)
+        {
+            owner = player;
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            var squares = new List<Square>();
+
+            if (this.owner == Player.White)
+            {
+                squares.Add(new Square(currentSquare.Row-1,currentSquare.Col));
+            }
+            else
+            {
+                squares.Add(new Square(currentSquare.Row+1, currentSquare.Col));
+            }
+
+            return squares;
         }
     }
 }
